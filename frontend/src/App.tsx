85619dc30b427b24c2605d504ab31fcd92d5c3b8
andrew-pavlov-ua <pavlovandreew@gmail.com>
2025-05-28 21:27:33 +0300

    HTTPS configuration added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,16 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const environment = import.meta.env.ENVIRONMENT;
+  const endpoint =
+  environment === "production"
+    ? "https://domain.com:4431"
+    : "http://localhost:8080";
+
   const [message, setMessage] = useState<string>("Loading...");
 
   useEffect(() => {
-    fetch("http://localhost:8080/hello")
+    fetch(endpoint + "/hello")
       .then((res) => res.json())
       .then((data) => setMessage(data.message))
       .catch((err) => console.error(err));
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
+import path from 'path';
+
+const isProduction = process.env.ENVIRONMENT === 'production';
+const tls_certs_path = path.resolve(__dirname, process.env.TLS_CERTIFICATES_DIR || 'tls_certs');
+const keyPath = path.join(tls_certs_path, 'privkeykey.pem');
+const certPath = path.join(tls_certs_path, 'cert.pem');
 
 // https://vite.dev/config/
+const serverConfig: any = {
+  port: 3000,
+};
+
+if (isProduction) {
+  serverConfig.https = {
+    key: fs.readFileSync(path.resolve(__dirname, keyPath)),
+    cert: fs.readFileSync(path.resolve(__dirname, certPath)),
+  };
+}
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: serverConfig,
+});
